d0919369a0a551d56b27a9f7dcd42b3867525667
Muhammad Bilal <bilal.se74@gmail.com>
2023-07-17 09:08:39 +0500

    completed most of the UI

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -9,8 +9,8 @@ class Calculator extends StatefulWidget {
 }
 
 class _CalculatorState extends State<Calculator> {
-  String userInput = "";
-  String result = "";
+  String userInput = "input";
+  String result = "0";
 
   List<String> buttonList = [
     "AC",
@@ -42,20 +42,100 @@ class _CalculatorState extends State<Calculator> {
         body: Column(
           children: [
             SizedBox(
-                height: MediaQuery.of(context).size.height / 3,
-                child: Column(
-                  children: [
-                    Container(
-                      padding: const EdgeInsets.all(20),
-                      alignment: Alignment.centerRight,
-                      child: const Text(
-                        "Input",
-                        style: TextStyle(fontSize: 32, color: Colors.white),
+              height: MediaQuery.of(context).size.height / 3,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(20),
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      userInput,
+                      style: TextStyle(
+                        fontSize: 32,
+                        color: Colors.white,
                       ),
                     ),
-                  ],
-                )),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(10),
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      result,
+                      style: TextStyle(
+                        fontSize: 48,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Container(
+                decoration: const BoxDecoration(
+                    color: gridColor,
+                    borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(30),
+                    )),
+                height: MediaQuery.of(context).size.height,
+                padding: const EdgeInsets.all(10),
+                child: GridView.builder(
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: buttonList.length,
+                    shrinkWrap: true,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      childAspectRatio: 1.3,
+                      crossAxisCount: 4,
+                      crossAxisSpacing: 12,
+                      mainAxisSpacing: 12,
+                    ),
+                    itemBuilder: (BuildContext context, index) {
+                      return customButton(buttonList[index]);
+                    }),
+              ),
+            ),
           ],
         ));
+  }
+
+  Widget customButton(String text) {
+    return Container(
+      decoration: BoxDecoration(
+          color: buttonColorDark, borderRadius: BorderRadius.circular(10)),
+      child: InkWell(
+        //splashColor: Colors.red,
+        onTap: () {},
+        child: Center(
+          child: Text(
+            text,
+            style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: getColor(text)),
+          ),
+        ),
+      ),
+    );
+  }
+
+  getColor(String text) {
+    switch (text) {
+      case "AC":
+      case "(":
+      case ")":
+        return greenColor;
+
+      case "/":
+      case "*":
+      case "+":
+      case "-":
+      case "=":
+        return redColor;
+      default:
+        return Colors.white;
+    }
   }
 }
--- lib/color.dart
+++ lib/color.dart
@@ -1,5 +1,7 @@
-
 import 'dart:ui';
 
-const backgroundColorDark =  Color(0x0000ed25); 
- 
+const backgroundColorDark = Color.fromRGBO(34, 37, 45, 1);
+const gridColor = Color.fromRGBO(41, 45, 54, 1);
+const buttonColorDark = Color.fromRGBO(39, 43, 51, 1);
+const greenColor = Color.fromRGBO(38, 239, 203, 1);
+const redColor = Color.fromRGBO(226, 99, 100, 1);
